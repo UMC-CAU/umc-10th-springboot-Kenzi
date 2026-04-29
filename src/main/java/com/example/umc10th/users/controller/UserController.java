882414055f92d8a8cdcb0f2f5563dd2e9da989bf
Lidohyeon <ehgus3193@naver.com>
2026-04-29 23:18:16 +0900

    feat: UserController

--- src/main/java/com/example/umc10th/users/controller/UserController.java
+++ src/main/java/com/example/umc10th/users/controller/UserController.java
@@ -1,28 +1,31 @@
 package com.example.umc10th.users.controller;
 
-import com.example.umc10th.users.entity.User;
+import com.example.umc10th.global.code.BaseSuccessCode;
+import com.example.umc10th.global.code.GeneralSuccessCode;
+import com.example.umc10th.global.response.ApiResponse;
+import com.example.umc10th.users.dto.UserReqDTO;
+import com.example.umc10th.users.dto.UserResDTO;
 import com.example.umc10th.users.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Tag(name = "Users", description = "유저 API")
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "전체 유저 조회", description = "등록된 모든 유저를 조회합니다.")
-    @GetMapping
-    public ResponseEntity<List<User>> getAll() {
-        return ResponseEntity.ok(userService.getAll());
+    @PostMapping("/me")
+    public ApiResponse<UserResDTO.GetInfo> me( @RequestBody UserReqDTO.GetInfo reqDTO ) {
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.success( code , userService.getMe(reqDTO.userId().toString()));
     }
 }

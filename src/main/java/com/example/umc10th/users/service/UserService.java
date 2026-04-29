89c841fd942ec96e81dcd134afc55bb742d482f2
Lidohyeon <ehgus3193@naver.com>
2026-04-29 23:18:23 +0900

    feat: UserService

--- src/main/java/com/example/umc10th/users/service/UserService.java
+++ src/main/java/com/example/umc10th/users/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.umc10th.users.service;
 
+import com.example.umc10th.global.exception.ProjectException;
+import com.example.umc10th.users.dto.UserResDTO;
 import com.example.umc10th.users.entity.User;
+import com.example.umc10th.users.enums.UserErrorCode;
 import com.example.umc10th.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,17 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public List<User> getAll() {
-        return userRepository.findAll();
+    public UserResDTO.GetInfo getMe(String userId) {
+        return userRepository.findById(userId).map(user -> new UserResDTO.GetInfo(
+                user.getId(),
+                user.getAddressCode(),
+                user.getName(),
+                user.getEmail(),
+                user.getAge(),
+                user.getRole(),
+                user.getPoint(),
+                user.getCreatedAt(),
+                user.getDeletedAt()
+        )).orElseThrow(() -> new ProjectException(UserErrorCode.USER_NOT_FOUND));
     }
 }

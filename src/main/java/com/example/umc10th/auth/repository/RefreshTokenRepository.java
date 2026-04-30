bd56a58e27faab2e7d59434e521142113b1fb08e
Lidohyeon <ehgus3193@naver.com>
2026-04-30 17:15:59 +0900

    refactor: auth 도메인 제거 -> User 도메인에 포함시키기

--- src/main/java/com/example/umc10th/auth/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.umc10th.auth.repository;
-
-import com.example.umc10th.auth.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-}

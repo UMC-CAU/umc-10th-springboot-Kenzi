06ae625f249687cd424356f179f597ad7f6dca4b
Lidohyeon <ehgus3193@naver.com>
2026-04-30 16:58:02 +0900

    feat : 유저 컨트롤러 swagger 추가하기

--- src/main/java/com/example/umc10th/users/controller/UserController.java
+++ src/main/java/com/example/umc10th/users/controller/UserController.java
@@ -24,21 +24,25 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "내 정보 조회", description = "유저 ID로 내 정보를 조회합니다.")
     @PostMapping("/me")
     public ApiResponse<UserResDTO.GetInfo> me(@RequestBody UserReqDTO.GetInfo reqDTO) {
         return ApiResponse.success(UserSuccessCode.USER_SUCCESS_FOUND, userService.getMe(reqDTO.userId()));
     }
 
+    @Operation(summary = "회원가입", description = "신규 유저를 회원가입 처리합니다.")
     @PostMapping("/signup")
     public ApiResponse<UserResDTO.GetInfo> signup(@RequestBody UserReqDTO.Signup reqDTO) {
         return ApiResponse.success(UserSuccessCode.USER_SUCCESS_SINGUP, userService.signUp(reqDTO));
     }
 
+    @Operation(summary = "로그인", description = "이메일과 비밀번호로 로그인합니다.")
     @PostMapping("/login")
     public ApiResponse<UserResDTO.GetInfo> login(@RequestBody UserReqDTO.Login reqDTO) {
         return ApiResponse.success(UserSuccessCode.USER_SUCCESS_LOGIN, userService.login(reqDTO));
     }
 
+    @Operation(summary = "회원 탈퇴", description = "유저를 탈퇴 처리합니다.")
     @PatchMapping("/witdraw")
     public ApiResponse<UserResDTO.GetInfo> withdraw(@RequestBody UserReqDTO.GetInfo reqDTO) {
         return ApiResponse.success(UserSuccessCode.USER_SUCCESS_WITHDRAW, userService.withdraw(reqDTO.userId()));

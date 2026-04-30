22fec98afd9607ea70aca6d3ad7e76b530290321
Lidohyeon <ehgus3193@naver.com>
2026-04-30 16:49:35 +0900

    feat : Auth 관련 성공 코드

--- src/main/java/com/example/umc10th/users/enums/UserSuccessCode.java
+++ src/main/java/com/example/umc10th/users/enums/UserSuccessCode.java
@@ -10,8 +10,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum UserSuccessCode implements BaseSuccessCode {
-    USER_SUCCESS_FOUND(HttpStatus.OK, "U200", "사용자 정보를 성공적으로 조회하였습니다."),
-    ;
+    USER_SUCCESS_FOUND(HttpStatus.OK, "사용자 정보를 성공적으로 조회하였습니다." , "U200"),
+    USER_SUCCESS_SINGUP(HttpStatus.OK , "사용자 회원가입이 성공적으로 마무리되었습니다." ,"U300"),
+    USER_SUCCESS_LOGIN(HttpStatus.OK, "로그인이 성공적으로 처리되었습니다.", "U301"),
+    USER_SUCCESS_WITHDRAW(HttpStatus.OK, "회원 탈퇴가 성공적으로 처리되었습니다.", "U302");
 
     private final HttpStatus httpStatus;
     private final String message;

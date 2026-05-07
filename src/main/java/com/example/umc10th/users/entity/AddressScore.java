c163e8fa6e23a5eb7a54d781d3572359ce2ca8f2
Lidohyeon <ehgus3193@naver.com>
2026-05-07 15:44:12 +0900

    feat: score_address 테이블 엔티티 생성 (복합키의 경우 embeddedId 클래스를 선언하여 관리.)

---
+++ src/main/java/com/example/umc10th/users/entity/AddressScore.java
@@ -0,0 +1,37 @@
+package com.example.umc10th.users.entity;
+
+import com.example.umc10th.reference.entity.Address;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "score_address")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AddressScore {
+
+    @EmbeddedId
+    private AddressScoreId id;
+
+    @MapsId("addressCode")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "code", referencedColumnName = "code", nullable = false)
+    private Address address;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(name = "score", nullable = false)
+    private Integer score = 0;
+}

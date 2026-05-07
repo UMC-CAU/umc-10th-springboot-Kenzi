c4ab0c790d078620569eb425848cf20822a36667
Lidohyeon <ehgus3193@naver.com>
2026-05-07 16:07:25 +0900

    feat: 미션 도메인 수행중 , 완료한 미션 조회하는 api 에러코드

--- src/main/java/com/example/umc10th/missions/enums/MissionErrorCode.java
+++ src/main/java/com/example/umc10th/missions/enums/MissionErrorCode.java
@@ -18,7 +18,8 @@ public enum MissionErrorCode implements BaseErrorCode {
     INVALID_PAGINATION_REQUEST(HttpStatus.BAD_REQUEST, "M404", "유효하지 않은 페이지 요청입니다."),
     MISSION_DOING_NOT_FOUND(HttpStatus.NOT_FOUND, "M405", "진행 중인 미션이 없습니다."),
     MISSION_DONE_NOT_FOUND(HttpStatus.NOT_FOUND, "M406", "완료한 미션이 없습니다."),
-    MISSION_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "M407", "유저를 찾을 수 없습니다.");
+    MISSION_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "M407", "유저를 찾을 수 없습니다."),
+    AVAILABLE_MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "M408", "수락 가능한 미션이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;

bb3c843170ac29caaac9e96a7236df7e20658b77
Lidohyeon <ehgus3193@naver.com>
2026-05-10 18:32:26 +0900

    refactor: review dto 수정

--- src/main/java/com/example/umc10th/missions/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/missions/dto/MissionReqDTO.java
@@ -21,21 +21,22 @@ public class MissionReqDTO {
             Integer page,
             @Schema(description = "페이지 크기(미입력 시 10)", example = "10")
             Integer size
-    ) {}
+    ) {
+    }
 
-    public record CompleteMissionsRequest(Long userId, Long missionId) {}
+    public record CompleteMissionsRequest(Long userId, Long missionId) {
+    }
 
-    public record PostMissionReviewRequest(Long userId, Long missionId, ReviewRequest review) {}
+    public record PostMissionReviewRequest(Long userId, Long missionId, ReviewRequest review) {
+    }
 
     public record ReviewRequest(
-            Long id,
-            Long missionId,
-            Long storeId,
-            Long userId,
-            String description,
-            BigDecimal score,
-            String photoUrl,
-            LocalDateTime createdAt,
-            LocalDateTime deletedAt
-    ) {}
+            @Schema(description = "미션 ID", example = "1") Long missionId,
+            @Schema(description = "스토어 ID", example = "10") Long storeId,
+            @Schema(description = "유저 ID", example = "2") Long userId,
+            @Schema(description = "리뷰 내용", example = "좋은 경험이었습니다.") String description,
+            @Schema(description = "점수", example = "4.5") BigDecimal score,
+            @Schema(description = "사진 URL", example = "http://example.com/photo.jpg") String photoUrl
+    ) {
+    }
 }

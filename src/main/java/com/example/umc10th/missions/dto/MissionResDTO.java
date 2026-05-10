3c3ef36bf1b938927e4f848c86d6490460902172
Lidohyeon <ehgus3193@naver.com>
2026-05-11 02:13:20 +0900

    feat : 페이지네이션 dto

--- src/main/java/com/example/umc10th/missions/dto/MissionResDTO.java
+++ src/main/java/com/example/umc10th/missions/dto/MissionResDTO.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 public class MissionResDTO {
 
+    public record Pagination<T>(
+            List<T> data,
+            Integer pageNumber,
+            Integer pageSize,
+            Long totalElements,
+            Integer totalPages,
+            Boolean last
+    ) {}
+
     public record AvailableMission(
             Long missionId,
             Integer point,
@@ -13,9 +22,9 @@ public class MissionResDTO {
 
     public record GetMissionListResponse(String addressCode, Long userId, List<AvailableMission> missions) {}
 
-    public record GetMissionDoneResponse(Long address, Long userId, List<AvailableMission> completedMissions) {}
+    public record GetMissionDoneResponse(Long address, Long userId, Pagination<AvailableMission> completedMissions) {}
 
-    public record GetMissionDoingResponse(Long address, Long userId, List<AvailableMission> doingMissions) {}
+    public record GetMissionDoingResponse(Long address, Long userId, Pagination<AvailableMission> doingMissions) {}
 
     public record SetMissionDoneResponse(Long userId, Long missionId, boolean completed) {}
 

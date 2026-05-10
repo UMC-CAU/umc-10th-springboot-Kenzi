5fbdbb4e9d2fac96efdb5bdbf7842f55465bcb44
Lidohyeon <ehgus3193@naver.com>
2026-05-11 02:37:32 +0900

    feat : 리뷰 에러 및 성공 코드 추가

--- src/main/java/com/example/umc10th/missions/enums/MissionErrorCode.java
+++ src/main/java/com/example/umc10th/missions/enums/MissionErrorCode.java
@@ -19,7 +19,8 @@ public enum MissionErrorCode implements BaseErrorCode {
     MISSION_DOING_NOT_FOUND(HttpStatus.NOT_FOUND, "M405", "진행 중인 미션이 없습니다."),
     MISSION_DONE_NOT_FOUND(HttpStatus.NOT_FOUND, "M406", "완료한 미션이 없습니다."),
     MISSION_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "M407", "유저를 찾을 수 없습니다."),
-    AVAILABLE_MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "M408", "수락 가능한 미션이 없습니다.");
+    AVAILABLE_MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "M408", "수락 가능한 미션이 없습니다."),
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "M409", "리뷰를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/example/umc10th/missions/enums/MissionSuccessCode.java
+++ src/main/java/com/example/umc10th/missions/enums/MissionSuccessCode.java
@@ -15,7 +15,8 @@ public enum MissionSuccessCode implements BaseSuccessCode {
     MISSION_DOING_FOUND(HttpStatus.OK, "M201", "진행 중 미션을 성공적으로 조회했습니다."),
     MISSION_DONE_FOUND(HttpStatus.OK, "M202", "완료 미션을 성공적으로 조회했습니다."),
     MISSION_COMPLETED(HttpStatus.OK, "M203", "미션 완료 처리에 성공했습니다."),
-    MISSION_REVIEW_CREATED(HttpStatus.CREATED, "M204", "미션 리뷰 등록에 성공했습니다.");
+    MISSION_REVIEW_CREATED(HttpStatus.CREATED, "M204", "미션 리뷰 등록에 성공했습니다."),
+    MISSION_REVIEW_FOUND(HttpStatus.OK, "M205", "미션 리뷰를 성공적으로 조회했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;

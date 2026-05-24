a9f1cb29234502b59b309b8090ad6d67b5b2d852
Lidohyeon <ehgus3193@naver.com>
2026-05-25 00:10:47 +0900

    refactor: 마이페이지 컨트롤러 수정

--- src/main/java/com/example/umc10th/users/controller/UserController.java
+++ src/main/java/com/example/umc10th/users/controller/UserController.java
@@ -3,25 +3,29 @@ package com.example.umc10th.users.controller;
 import com.example.umc10th.global.response.ApiResponse;
 import com.example.umc10th.users.dto.UserReqDTO;
 import com.example.umc10th.users.dto.UserResDTO;
+import com.example.umc10th.users.entity.AuthMember;
 import com.example.umc10th.users.enums.UserSuccessCode;
 import com.example.umc10th.users.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Users", description = "유저 API")
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "JWT TOKEN")
 public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "내 정보 조회", description = "유저 ID로 내 정보를 조회합니다.")
+    @Operation(summary = "내 정보 조회", description = "JWT 토큰으로 내 정보를 조회합니다.")
     @PostMapping("/me")
-    public ApiResponse<UserResDTO.GetInfoResponse> me(@RequestBody UserReqDTO.GetInfoRequest reqDTO) {
-        return ApiResponse.success(UserSuccessCode.USER_SUCCESS_FOUND, userService.getMe(reqDTO.userId()));
+    public ApiResponse<UserResDTO.GetInfoResponse> me(@AuthenticationPrincipal AuthMember authMember) {
+        return ApiResponse.success(UserSuccessCode.USER_SUCCESS_FOUND, userService.getMe(authMember));
     }
 
     @Operation(summary = "회원가입", description = "신규 유저를 회원가입 처리합니다.")
@@ -32,7 +36,7 @@ public class UserController {
 
     @Operation(summary = "로그인", description = "이메일과 비밀번호로 로그인합니다.")
     @PostMapping("/login")
-    public ApiResponse<UserResDTO.GetInfoResponse> login(@RequestBody UserReqDTO.LoginRequest reqDTO) {
+    public ApiResponse<UserResDTO.LoginResponse> login(@RequestBody UserReqDTO.LoginRequest reqDTO) {
         return ApiResponse.success(UserSuccessCode.USER_SUCCESS_LOGIN, userService.login(reqDTO));
     }
 
